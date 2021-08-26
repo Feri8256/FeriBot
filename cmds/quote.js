2c6c8f57a512881f903a39f1d19fc18f7bdc9b86
Feri8256 <75679256+Feri8256@users.noreply.github.com>
2021-08-26 20:59:47 +0200

    Idézetek működnek most már megint...

--- cmds/quote.js
+++ cmds/quote.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
-
+const findLanguage = require('../findLanguage');
 module.exports = {
     name:'quote',
     categories: ['fun'],
     aliases: ['citatum'],
     description: ['Küld egy idézetet.', 'Sends a quote.'],
     execute(Discord, client, message, args, L, DataMgr, ErrMessages) {
+        let langSetting = findLanguage(client, message.guild.id);
+
         let idezetek = require(`../json/idezetek-${langSetting}.json`);
         let rdmIdezetNum = Math.floor(Math.random() * idezetek.length);
     
